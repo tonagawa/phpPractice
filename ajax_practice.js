41dbea302e56f42d1a8ac6e1f77189c10dea6da3
t.ogawa <97233360+tonagawa@users.noreply.github.com>
2022-03-12 18:02:21 +0900

    コメントの書き直し

--- ajax_practice.js
+++ ajax_practice.js
@@ -1,24 +1,33 @@
 function OutputData()
 {
-    //ポスト用の連想配列を用意
+    // ポスト用の連想配列を用意
     let postData = {};
     postData["proc"]       = "setdata";
+    
+    // 非同期処理の実行
     $.ajax({
         url : "db_connect.php",
         type: "POST",
         data: postData,
         dataType: "json",
     })
+    
+    // 通信成功時
     .done(function (data) {
+        
+        // データをJSON形式に変換
         let data_stringify = JSON.stringify(data);
         let data_json = JSON.parse(data_stringify);
 
+        // HTMLに反映
         $('#input1').val(data_json['text']);
         $('#input2').val(data_json['text']);
         $('#input3').val(data_json['text']);
         $('#input4').val(data_json['text']);
         $('#input5').val(data_json['text']);
     })
+    
+    // 通信失敗時
     .fail(function (data) {
         alert("接続エラー");
     });
@@ -26,25 +35,35 @@ function OutputData()
 
 function SearchData()
 {
+    // ポスト用の連想配列を用意
     let postData = {};
     postData['proc'] = 'searchdata';
     postData['search_text'] = $('#search_text').val();
+    
+    // 非同期処理の実行
     $.ajax({
         url : "db_connect.php",
         type: "POST",
         data: postData,
         dataType: "json",
     })
+    
+    // 通信成功時
     .done(function(data){
+        
+        // データをJSON形式に変換
         let data_stringify = JSON.stringify(data);
         let data_json = JSON.parse(data_stringify);
 
+        // HTMLに反映
         $('#input1').val(data_json['text']);
         $('#input2').val(data_json['text']);
         $('#input3').val(data_json['text']);
         $('#input4').val(data_json['text']);
         $('#input5').val(data_json['text']);
     })
+    
+    // 通信失敗時
     .fail(function(data){
         alert("接続エラー");
     });
